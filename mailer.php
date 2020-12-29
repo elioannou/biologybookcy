1aa54b32be63891a5f51b834e9c6f7a9b3557519
Eleftherios Ioannou <ei233@pip.srcf.societies.cam.ac.uk>
2020-12-29 00:07:27 +0000

    Updated form

--- mailer.php
+++ mailer.php
@@ -7,50 +7,55 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
         $name = strip_tags(trim($_POST["name"]));
-				$name = str_replace(array("\r","\n"),array(" "," "),$name);
+        $name = str_replace(array("\r","\n"),array(" "," "),$name);
+        $phone = strip_tags(trim($_POST["phone"]));
+        $acs = strip_tags(trim($_POST["acs"]));
+        $items = strip_tags(trim($_POST["items"]));
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
         // $cont_subject = trim($_POST["subject"]);
         $message = trim($_POST["message"]);
 
         // Check that data was sent to the mailer.
-        if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        if ( empty($name) OR empty($phone) OR empty($acs) OR ( !empty($email) AND !filter_var($email, FILTER_VALIDATE_EMAIL))) {
             // Set a 400 (bad request) response code and exit.
             http_response_code(400);
-            echo "Oops! There was a problem with your submission. Please complete the form and try again.";
+            echo "Υπάρχει κάποιο λάθος στη φόρμα. Παρακαλώ συμπληρώστε σωστά τα απαιτούμενα στοιχεία.";
             exit;
         }
 
         // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
         $recipient = "sendtolefteris@gmail.com";
 
         // Set the email subject.
-        $subject = "New contact from $name";
+        $subject = "New order from $name $phone";
 
         // Build the email content.
         $email_content = "Name: $name\n";
+        $email_content .= "Phone: $phone\n";
+        $email_content .= "ACS Store: $acs\n";
+        $email_content .= "Items: $items\n";
         $email_content .= "Email: $email\n\n";
         // $email_content .= "Subject: $cont_subject\n";
         $email_content .= "Message:\n$message\n";
 
         // Build the email headers.
-        $email_headers = "From: New book order <order@book.com>";
+        $email_headers = "From: Book order <order@book.com>";
 
         // Send the email.
         if (mail($recipient, $subject, $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
             http_response_code(200);
-            echo "Thank You! Your message has been sent.";
+            echo "Ευχαριστούμε! Η παραγγελία σας έχει σταλεί.";
         } else {
             // Set a 500 (internal server error) response code.
             http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
+            echo "Δυστυχώς η παραγγελία σας δεν έχει σταλεί (Error 500). Παρακαλώ δοκιμάστε αργότερα.";
         }
 
     } else {
         // Not a POST request, set a 403 (forbidden) response code.
         http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
+        echo "Δυστυχώς η παραγγελία σας δεν έχει σταλεί (Error 403). Παρακαλώ δοκιμάστε αργότερα.";
     }
 
 ?>
